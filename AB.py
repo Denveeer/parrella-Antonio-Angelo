e627bcab9049a8fa7be3a3bd8a54762ef163362d
Denveeer <91076161+Denveeer@users.noreply.github.com>
2021-10-25 18:54:32 +0200

    Add files via upload

---
+++ AB.py
@@ -0,0 +1,6 @@
+A=int(input('inserisci un numero:'))
+B=int(input('inserisci un secondo numero:'))
+if A > B :
+    print ('il numero minore è' + str(B))
+else:
+    print('il numero minore è' + str (A))
