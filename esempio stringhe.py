58388602b1bce1877d371443b528960e96862113
Denveeer <91076161+Denveeer@users.noreply.github.com>
2022-10-17 22:58:04 +0200

    Add files via upload

---
+++ esempio stringhe.py
@@ -0,0 +1,15 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> A = "bottone"
+>>> E = "frullatore"
+>>> I = "Captain america"
+>>> O = "rudolf"
+>>> U = "Thor dio del tuono"
+>>> print(I) print(U)
+SyntaxError: invalid syntax
+>>> A = "bottone"
+>>> print(I)
+Captain america
+>>> print(U)
+Thor dio del tuono
+>>> 
